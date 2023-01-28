aae99dcd356e6260ee5fd8db10f3ac13c5997f2f
guiac <gui.acassemiro@gmail.com>
2023-01-28 16:17:55 -0300

    :construction: removing unecessary files

--- src/main/routes/account.routes.ts
+++
@@ -1,9 +0,0 @@
-import { adaptRoute } from '@/main/adapters'
-import { makeSignUpController } from '@/main/factories'
-
-import { Router } from 'express'
-import { auth } from '@/main/middlewares'
-
-export default (router: Router): void => {
-    router.post('/signup', adaptRoute(makeSignUpController()))
-}
