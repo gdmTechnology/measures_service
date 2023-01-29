66c5f6500b1a7f2d2f8da4357047608884ab9004
guiac <gui.acassemiro@gmail.com>
2023-01-29 14:49:25 -0300

    :sparkles: SaveSensorMeasureRepository list()

--- src/infra/db/mongodb/sensor.measure.mongo.repository.ts
+++ src/infra/db/mongodb/sensor.measure.mongo.repository.ts
@@ -1,9 +1,18 @@
-import { SaveSensorMeasureRepository } from '@/data/protocols'
+import { SaveSensorMeasureRepository, ListSensorMeasureRepository } from '@/data/protocols'
 import { SensorMeasureModel } from './models'
 
-export class SensorMeasureMongoRepository implements SaveSensorMeasureRepository {
+export class SensorMeasureMongoRepository implements SaveSensorMeasureRepository, ListSensorMeasureRepository {
     async save(data: SaveSensorMeasureRepository.Params): Promise<SaveSensorMeasureRepository.Result> {
         const model = new SensorMeasureModel(data)
         return await model.save()
+    }
+
+    async list(data: ListSensorMeasureRepository.Params): Promise<ListSensorMeasureRepository.Result[]> {
+        const { limit, page, sensorIdentification } = data
+        const list = await SensorMeasureModel.find({ sensorIdentification })
+            .limit(limit * 1)
+            .skip((page - 1) * limit)
+            .exec()
+        return list
     }
 }
