e6eb29e5c768e7577a7b8f0dfef127204f97b4f6
guiac <gui.acassemiro@gmail.com>
2023-01-31 20:26:46 -0300

    :wrench: setup env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,11 @@
 export default {
   port: process.env.PORT || 3002,
   jwtSecret: process.env.JWT_SECRET || '1kZDnw8==jh',
-  mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/rem'
+  mongoUrl: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}?authSource=admin` || 'mongodb://rem:rem2023@mongo:27017/rem?authSource=admin',
+  adminEmail: process.env.ADMIN_EMAIL || 'gui.acassemiro@gmail.com',
+  mongoHost: process.env.MONGO_HOST || 'mongo',
+  mongoPort: process.env.MONGO_PORT || 27017,
+  mongoDbName: process.env.MONGO_DB_NAME || 'rem',
+  mongoPass: process.env.MONGO_PASS || 'rem2023',
+  mongoUser: process.env.MONGO_USER || 'rem'
 }
